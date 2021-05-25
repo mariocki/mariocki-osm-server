01aefb4cbc69d945e392c49aacf16b44c6f7d5db
mariocki@gmail.com <mariocki@gmail.com>
2021-05-25 14:30:44 +0100

    updates to carto

--- scripts/maps/run.sh
+++ scripts/maps/run.sh
@@ -28,13 +28,13 @@ service cron restart
 # render_list_geo.pl -n 4 -z 7 -Z 9 -x -9.5 -X 2.72 -y 49.39 -Y 61.26 -m ajt
 
 # London
-# render_list_geo.pl -f -n 4 -z 14 -Z 16 -x -7.4 -X 0.57 -y 51.29 -Y 51.8 -m ajt
+# render_list_geo.pl -f -n 2 -z 8 -Z 16 -x -7.4 -X 0.57 -y 51.29 -Y 51.8 -m ajt
 
 # stanstead airport
 # render_list_geo.pl -f -n 4 -z 6 -Z 16 -x 0.05 -X 0.42 -y 51.80 -Y 51.92 -m ajt
 
 # manchester
-# render_list_geo.pl -f -n 2 -z 8 -Z 10 -x -2.5 -X -1.99 -y 53.36 -Y 53.61 -m ajt
+# render_list_geo.pl -f -n 2 -z 8 -Z 16 -x -2.5 -X -1.99 -y 53.36 -Y 53.61 -m ajt
 
 sleep infinity
 
