be5bfb2fac70cb407e49e5b97a7660fa31400f34
mariocki@gmail.com <mariocki@gmail.com>
2021-05-31 16:54:30 +0100

    tweak pghero setup

--- scripts/db/init-user-db.sh
+++ scripts/db/init-user-db.sh
@@ -5,4 +5,16 @@ export PGUSER="$POSTGRES_USER"
 
 "${psql[@]}" --dbname="$POSTGRES_DB" <<-EOSQL
   CREATE EXTENSION IF NOT EXISTS hstore;
+
+  CREATE TABLE "pghero_query_stats" (
+    "id" bigserial primary key,
+    "database" text,
+    "user" text,
+    "query" text,
+    "query_hash" bigint,
+    "total_time" float,
+    "calls" bigint,
+    "captured_at" timestamp
+  );
+  CREATE INDEX ON "pghero_query_stats" ("database", "captured_at");
 EOSQL
