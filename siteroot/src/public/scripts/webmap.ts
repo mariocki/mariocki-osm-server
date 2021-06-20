81939859fa71fb893c9fa7011514a45f00751530
mariocki@gmail.com <mariocki@gmail.com>
2021-06-20 01:40:30 +0100

    tweak UI

--- siteroot/src/public/scripts/webmap.ts
+++ siteroot/src/public/scripts/webmap.ts
@@ -45,8 +45,7 @@ const AzureMaps_Imagery = L.tileLayer(
 const openAIP = L.tileLayer(
     "http://{s}.tile.maps.openaip.net/geowebcache/service/tms/1.0.0/openaip_basemap@EPSG%3A900913@png/{z}/{x}/{y}.{ext}",
     {
-        attribution:
-            '<a href="https://www.openaip.net/">openAIP Data</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-NC-SA</a>)',
+        attribution: '<a href="https://www.openaip.net/">openAIP Data</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-NC-SA</a>)',
         ext: "png",
         minZoom: 9,
         maxZoom: 14,
@@ -59,69 +58,36 @@ const openAIP = L.tileLayer(
 const openSeaMap = L.tileLayer(
     "https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png",
     {
-        attribution:
-            'Map data: &copy; <a href="http://www.openseamap.org">OpenSeaMap</a> contributors',
-    }
+        attribution: 'Map data: &copy; <a href="http://www.openseamap.org">OpenSeaMap</a> contributors',
+        minZoom: 9,
+    }as L.TileLayerOptions
+);
+
+const openWeatherMapTemp = L.tileLayer(
+    "https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid={openWeatherMapsAPIkey}",
+    {
+        attribution: 'Map data: &copy; <a href="http://www.openweathermap.org">OpenWeatherMap</a> contributors',
+        openWeatherMapsAPIkey: process.env.OWM_MAP_KEY
+    } as L.TileLayerOptions
 );
 
 const AzureMaps_MicrosoftWeatherRadarMain = L.tileLayer(
     "https://atlas.microsoft.com/map/tile?api-version={apiVersion}&tilesetId=microsoft.weather.radar.main&x={x}&y={y}&zoom={z}&language={language}&subscription-key={subscriptionKey}",
     {
-        attribution:
-            "See https://docs.microsoft.com/en-US/rest/api/maps/renderv2/getmaptilepreview#uri-parameters for details.",
+        attribution: "See https://docs.microsoft.com/en-US/rest/api/maps/renderv2/getmaptilepreview#uri-parameters for details.",
         apiVersion: "2.0",
         subscriptionKey: process.env.AZURE_MAP_KEY,
         language: "en-US",
     } as L.TileLayerOptions
 );
 
-let basemapLayer;
-let tileLayer;
+map.addLayer(osmBaseLayer);
+const baseMaps = {"Base":osmBaseLayer, "ESRIWorldImagery": Esri_WorldImagery, "AzureImagery": AzureMaps_Imagery }
+const overlayMaps = { "OpenAIP": openAIP, "OpenSeaMap": openSeaMap, "OpenWeatherMap Temp": openWeatherMapTemp, "AzureWeather": AzureMaps_MicrosoftWeatherRadarMain }
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
 let kmlLayer;
-
-function setTileLayer(tilelayer) {
-    if (tileLayer) {
-        map.removeLayer(tileLayer);
-    }
-
-    switch (tilelayer) {
-        case "OpenAIP":
-            map.addLayer(openAIP);
-            tileLayer = openAIP;
-            break;
-        case "OpenSeaMap":
-            map.addLayer(openSeaMap);
-            tileLayer = openSeaMap;
-            break;
-        case "AzureWeather":
-            map.addLayer(AzureMaps_MicrosoftWeatherRadarMain);
-            tileLayer = AzureMaps_MicrosoftWeatherRadarMain;
-            break;
-        default:
-            break;
-    }
-}
-
-function setBasemap(basemap) {
-    if (basemapLayer) {
-        map.removeLayer(basemapLayer);
-    }
-
-    switch (basemap) {
-        case "ESRIWorldImagery":
-            map.addLayer(Esri_WorldImagery);
-            basemapLayer = Esri_WorldImagery;
-            break;
-        case "AzureImagery":
-            map.addLayer(AzureMaps_Imagery);
-            basemapLayer = AzureMaps_Imagery;
-            break;
-        default:
-            map.addLayer(osmBaseLayer);
-            basemapLayer = osmBaseLayer;
-            break;
-    }
-}
 
 function setKMLLayer(showKml) {
     if (kmlLayer) {
@@ -154,17 +120,7 @@ map.on("moveend", () => {
     )}, ${latlon.lng.toFixed(4)}`;
 });
 
-document.querySelector("#basemaps").addEventListener("change", (e) => {
-    const basemap = (e.target as HTMLSelectElement).value;
-    setBasemap(basemap);
-});
-document.querySelector("#tileLayer").addEventListener("change", (e) => {
-    const tilelayer = (e.target as HTMLSelectElement).value;
-    setTileLayer(tilelayer);
-});
 document.querySelector("#kml").addEventListener("change", function () {
     const showKml = this.checked;
     setKMLLayer(showKml);
 });
-
-setBasemap("None");
